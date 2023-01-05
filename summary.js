7e7e3d83c100f9837c1ecc83e28090dbd6b84944
al-mahfuz <almahfuzhossen@gmail.com>
2023-01-05 21:16:34 +0600

    summary

---
+++ summary.js
@@ -0,0 +1,14 @@
+/* 
+1. Find the documentation of console tab utilities API, and then read..
+2. Easiest way to find out the event listener attached to a dom element.
+3. How can you break the first line of a breakpoint. Rather you will use a console api utilities.
+
+----------------------
+
+1. what is accessibility?
+2. what is a call stack?
+3. what is the purpose of regular expression?
+4. what is the purpose of the lighthouse tab of chrome Dev Tool..
+
+-----------------------
+*/
