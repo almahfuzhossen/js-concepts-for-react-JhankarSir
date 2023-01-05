218516b69507f661ad278566b1a983b7f375bab2
al-mahfuz <almahfuzhossen@gmail.com>
2023-01-05 12:27:05 +0600

    storage

---
+++ storage.js
@@ -0,0 +1,14 @@
+const addTotalStorage = () => {
+    const idInput = document.getElementById('storage-id');
+    const id = idInput.value;
+
+    const valueInput = document.getElementById('storage-value');
+    const value = valueInput.value;
+
+    if( id && value ){
+    localStorage.setItem(id, value);
+
+    }
+    idInput.value = '';
+    valueInput.value = '';
+}
